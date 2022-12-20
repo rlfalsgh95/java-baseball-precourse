bafd7221e18a5205e0dda3997832fdfa44a426cc
rlfalsgh95 <rlfalsgh95@naver.com>
2022-12-21 00:50:44 +0900

    feat : 숫자야구게임을 실행하는 main 메서드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final int numberPitches = 3;
+        BaseballSimulator baseBallGameSimulator = new CLIBaseballSimulator(numberPitches);
+        baseBallGameSimulator.simulate();
     }
 }
